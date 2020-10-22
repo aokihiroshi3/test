6950cd955533f1336dda14ed4b80abe876c0e8e4
aokihiroshi3 <aoki.hiroshi3@kao.com>
2020-10-22 12:27:06 +0900

    tt

--- hellow_world.py
+++ hellow_world.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 
 a = 3
 print(a)
+plt.plot(aa, bb)
+
+plt.plot(xrange, y)
+
+
+
 
 aa = [2,3]
 bb = [3,2]
@@ -13,6 +19,7 @@ print('hello world')
 plt.plot(x, async y)
 
 plt.plot(a, aa)
+
 
 
 ((aa))
